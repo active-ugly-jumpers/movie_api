76ca6412156162fa00de16d95ce56deb2140620b
p <pasha.sem@tutamail.com>
2025-09-22 14:57:29 +0200

    Added error handling

--- index.js
+++ index.js
@@ -5,35 +5,75 @@ const express = require('express'),
 
 const app = express();
 
-let topBooks = [
+let topMovies = [
     {
-        title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J.K. Rowling'
+        title: 'No Other Land',
+        directors: 'Basel Adra, Hamdan Ballal, Yuval Abraham, Rachel Szor'
     },
     {
-        title: 'Lord of the Rings',
-        author: 'J.R.R. Tolkien'
+        title: 'Uncle Boonmee Who Can Recall His Past Lives',
+        directors: 'Apichatpong Weerasethakul'
     },
     {
-        title: 'Twilight',
-        author: 'Stephanie Meyer'
+        title: 'Tár',
+        directors: 'Todd Field'
+    },
+    {
+        title: 'Neptune Frost',
+        directors: 'Anisia Uzeyman, Saul Williams'
+    },
+    {
+        title: 'The Banshees of Inisherin',
+        directors: 'Martin McDonagh'
+    },
+    {
+        title: 'Evil Does Not Exist',
+        directors: 'Ryusuke Hamaguchi'
+    },
+    {
+        title: 'Blade Runner',
+        directors: 'Ridley Scott'
+    },
+    {
+        title: '2001: A Space Odyssey',
+        directors: 'Stanley Kubrick'
+    },
+    {
+        title: 'La Jetée',
+        directors: 'Chris Marker'
+    },
+    {
+        title: 'Nostalgia for the Light',
+        directors: 'Patricio Guzmán'
     }
 ];
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 app.use(morgan('common', { stream: accessLogStream }));
-// GET requests
+
 app.get('/', (req, res) => {
-    res.send('Welcome to my book club!');
+    res.send('Welcome to my movies club in the making!');
 });
+
 app.use(express.static('public'));
 
-app.get('/books', (req, res) => {
-    res.json(topBooks);
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
+app.use((err, req, res, next) => {
+    console.error('Error occurred:');
+    console.error('Time:', new Date().toISOString());
+    console.error('URL:', req.url);
+    console.error('Method:', req.method);
+    console.error('Error message:', err.message);
+    console.error('Stack trace:', err.stack);
+    console.error('---');
 
-// listen for requests
+    // Send error response to client
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
