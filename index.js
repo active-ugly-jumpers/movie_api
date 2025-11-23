2c127518188545622bfc3ef69dfa51d31b38fd54
p <pasha.sem@tutamail.com>
2025-11-24 00:04:08 +0100

    Updated cors

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // Connect to MongoDB using environment variable
-mongoose.connect(process.env.MONGODB_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const port = process.env.PORT || 8080;
@@ -47,7 +47,22 @@ app.use(limiter);
 app.use(express.json({ limit: '10mb' })); // Limit JSON payload size
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = [
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'https://active-ugly-jumpers.netlify.app'
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 let auth = require('./auth')(app); // Import auth.js file and pass in app
 const passport = require('passport');
